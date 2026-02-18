7f3b2c39c8e86705432897ccea9b7fa3d91994f2
Maksim Zayats <maximzayats1@gmail.com>
2026-02-18 21:44:12 +0400

    refactor: simplify type definitions and remove unused classes in sdef.py

--- src/openmac/_internal/sdef.py
+++ src/openmac/_internal/sdef.py
@@ -1,54 +1,10 @@
 from __future__ import annotations
 
 from collections import UserDict, UserString
-from typing import NamedTuple, NewType, TypeAlias
+from typing import Any, NamedTuple
 
 from pydantic import GetCoreSchemaHandler
 from pydantic_core import CoreSchema, core_schema
-
-
-class Text(UserString):
-    """A text returned from Apple Script, represented as a string."""
-
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls,
-        source_type: Any,
-        handler: GetCoreSchemaHandler,
-    ) -> CoreSchema:
-        return core_schema.no_info_after_validator_function(cls, handler(str))
-
-
-class Integer(int):
-    """An integer returned from Apple Script, represented as an int."""
-
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls,
-        source_type: Any,
-        handler: GetCoreSchemaHandler,
-    ) -> CoreSchema:
-        return core_schema.no_info_after_validator_function(cls, handler(int))
-
-
-class Real(float):
-    """A real number returned from Apple Script, represented as a float."""
-
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls,
-        source_type: Any,
-        handler: GetCoreSchemaHandler,
-    ) -> CoreSchema:
-        return core_schema.no_info_after_validator_function(cls, handler(float))
-
-
-Number: TypeAlias = Integer | Real
-"""A number returned from Apple Script, represented as an int or a float."""
-
-
-Boolean = NewType("Boolean", bool)
-"""A boolean returned from Apple Script, represented as a bool."""
 
 
 class Specifier(UserString):
@@ -75,7 +31,7 @@ class LocationSpecifier(Specifier):
         return core_schema.no_info_after_validator_function(cls, handler(str))
 
 
-class Record(UserDict[str, "Any"]):
+class Record(UserDict[str, Any]):
     """A record returned from Apple Script, represented as a dictionary."""
 
     @classmethod
@@ -125,19 +81,3 @@ class Rectangle(NamedTuple):
     top: int
     right: int
     bottom: int
-
-
-Any: TypeAlias = (
-    Text
-    | Integer
-    | Real
-    | Number
-    | Boolean
-    | Specifier
-    | LocationSpecifier
-    | Record
-    | Date
-    | File
-    | Point
-    | Rectangle
-)
