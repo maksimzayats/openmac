bac3620da98508a96b5260fc89a529c1172f3536
Maksim Zayats <maximzayats1@gmail.com>
2026-03-02 00:24:38 +0300

    feat: extend parser to include javascript_executor parameter in parse method

--- src/openmac/apps/browsers/pages/generic/parser.py
+++ src/openmac/apps/browsers/pages/generic/parser.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Callable
+from typing import Any
+
 from openmac.apps.browsers.pages.generic.structure import GenericPage
 
 
@@ -7,5 +10,9 @@ class GenericPageParser:
     def __init__(self, page_cls: type[GenericPage] = GenericPage) -> None:
         self._page_cls = page_cls
 
-    def parse(self, source: str) -> GenericPage:
+    def parse(
+        self,
+        source: str,
+        javascript_executor: Callable[[str], Any],
+    ) -> GenericPage:
         raise NotImplementedError
