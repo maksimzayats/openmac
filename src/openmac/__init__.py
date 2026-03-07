401ec2685f982d345b92e9fefe361109b60aa005
Maksim Zayats <maximzayats1@gmail.com>
2026-03-07 19:18:59 +0300

    feat: add Safari and Chrome browser objects to the application module

--- src/openmac/__init__.py
+++ src/openmac/__init__.py
@@ -1,3 +1,31 @@
 from __future__ import annotations
 
-__all__: list[str] = []
+from openmac.apps.browsers.base.objects.application import IBrowser
+from openmac.apps.browsers.base.objects.tabs import IBrowserTab
+from openmac.apps.browsers.base.objects.windows import IBrowserWindow
+from openmac.apps.browsers.chrome.objects.application import Chrome
+from openmac.apps.browsers.chrome.objects.bookmark_folders import (
+    ChromeBookmarkFolder,
+    ChromeBookmarkFoldersManager,
+)
+from openmac.apps.browsers.chrome.objects.tabs import ChromeTab
+from openmac.apps.browsers.chrome.objects.windows import ChromeWindow
+from openmac.apps.browsers.safari.objects.application import Safari
+from openmac.apps.browsers.safari.objects.documents import SafariDocument
+from openmac.apps.browsers.safari.objects.tabs import SafariTab
+from openmac.apps.browsers.safari.objects.windows import SafariWindow
+
+__all__: list[str] = [
+    "Chrome",
+    "ChromeBookmarkFolder",
+    "ChromeBookmarkFoldersManager",
+    "ChromeTab",
+    "ChromeWindow",
+    "IBrowser",
+    "IBrowserTab",
+    "IBrowserWindow",
+    "Safari",
+    "SafariDocument",
+    "SafariTab",
+    "SafariWindow",
+]
